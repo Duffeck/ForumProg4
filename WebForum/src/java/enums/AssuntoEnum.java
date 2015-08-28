e8fc0b48390ce88d77ad7ede027a45ecd2ddfe33
lucas.ribeiro <lucas.ribeiro@M05093.sjp.local>
2015-08-28 16:47:49 -0300

    Criando Classes e Servlets.

---
+++ WebForum/src/java/enums/AssuntoEnum.java
@@ -0,0 +1,39 @@
+/*
+ * Desenvolvido por Lucas Duffeck Ribeiro para a disciplina de Programacao 4
+ * BSI - PUC PR - 4o Per�odo.
+ */
+package enums;
+
+/**
+ *
+ * @author lucas.ribeiro
+ */
+public enum AssuntoEnum {
+    PC(1, "pc", "Jogos de Computador"),
+    JOGOSDEMESA(2, "jm", "Jogos de Mesa"),
+    PLAYSTATION(3, "ps", "Jogos de PlayStation"),
+    XBOX(4, "xb", "Jogos de Xbox"),
+    DS(5, "3d", "Jogos de 3DS");
+    
+    private int id;
+    private String sigla, descrição;
+    
+    private AssuntoEnum(int id, String sigla, String descrição){
+        this.id = id;
+        this.sigla = sigla;
+        this.descrição = descrição;
+    }
+    
+    @Override
+    public String toString(){
+        return this.descrição;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+}
---
+++ WebForum/web/WEB-INF/JSP/mainPage.jsp
@@ -0,0 +1,24 @@
+<%-- 
+    Document   : mainPage
+    Created on : 28/08/2015, 13:23:39
+    Author     : lucas.ribeiro
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Página Inicial - Web Fórum</title>
+    </head>
+    <body>
+        <div>
+            TheFórum
+        </div>
+        <div>
+            Lista de Assuntos
+        </div>
+        
+    </body>
+</html>
