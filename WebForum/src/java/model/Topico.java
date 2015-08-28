507978d11321dcc68bf9ab415ab46e9822933ab5
lucas.ribeiro <lucas.ribeiro@m05092.sjp.local>
2015-08-28 13:01:29 -0300

    Adicionadas classes: Topico e Mensagem.

---
+++ WebForum/src/java/model/Topico.java
@@ -0,0 +1,14 @@
+/*
+ * Desenvolvido por Lucas Duffeck Ribeiro para a disciplina de Programacao 4
+ * BSI - PUC PR - 4o Per�odo.
+ */
+
+package model;
+
+/**
+ *
+ * @author lucas.ribeiro
+ */
+public class Topico {
+    
+}
